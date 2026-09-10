dc9c4cea2e0d80bb51a7c08a2ac5b26ce1f16f0a
KristinaLebedevaa <lkslkslks2008@gmail.com>
2026-09-10 16:03:09 +0500

    Task6

---
+++ Tyuiu.LebedevaKS.Sprint0.Task6.V0/Program.cs
@@ -0,0 +1,15 @@
+﻿using Tyuiu.LebedevaKS.Sprint0.Task6.V0.Lib;
+namespace Tyuiu.LebedevaKS.Sprint0.Task6.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numsArray = new int[] { 1, 2, 3, 4, 5 };   
+            Console.WriteLine("сумма элементов массива = " + DataService.AdditionArray(numsArray));
+            Console.WriteLine("разность элементов массива = " + DataService.SubtructionArray(numsArray));
+            Console.WriteLine("произведение элементов массива = " + DataService.MultiplicationArray(numsArray));
+            Console.ReadKey();
+        }
+    }
+}
